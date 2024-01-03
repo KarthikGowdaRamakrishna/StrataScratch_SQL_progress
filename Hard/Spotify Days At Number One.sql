2632310252b1dbcbafc5e8a38a58efd0c6a70c58
KarthikGowdaRamakrishna <144963620+KarthikGowdaRamakrishna@users.noreply.github.com>
2024-01-03 14:34:40 -0500

    Update Spotify Days At Number One.sql

--- Hard/Spotify Days At Number One.sql
+++ Hard/Spotify Days At Number One.sql
@@ -1,1 +1,4 @@
+-------Question---------
+<br>--Find the number of days a US track has stayed in the 1st position for both the US and worldwide rankings on the same day.</br> Output the track name and the number of days in the 1st position. Order your output alphabetically by track name.If the region 'US' appears in dataset, it should be included in the worldwide ranking.
 
+Tables: spotify_daily_rankings_2017_us, spotify_worldwide_daily_song_ranking--
